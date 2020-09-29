69fc3b66d8c101b6158c379709a796a698edef73
cad2040 <cad2040@gmail.com>
2020-09-29 17:18:55 +0100

    dashboard GUI tab complete, now fetches all plot locations from DB and displays each in a i-frame

--- GUI/Homepage.php
+++ GUI/Homepage.php
@@ -55,18 +55,127 @@
       </div>
       </div>
       </div>
+
+      <!---Create Tab Buttons--->
+      <div class="tab">
+        <button class="tablinks" onclick="openDash(event, 'Dashboard')"
+                id="defaultOpen">Dashboard</button>
+        <button class="tablinks" onclick="openDash(event, 'Add Sensor')">
+        Add Sensor</button>
+        <button class="tablinks" onclick="openDash(event, 'Add Plant')">
+        Add Plant</button>
+        <button class="tablinks" onclick="openDash(event, 'Assign Sensor')">
+        Assign Sensor</button>
+      </div>
+
+      <?php
+        $url="MYSQL IP:3306";
+        $username="MYSQL UNAME";
+        $password="MYSQL PASS";
+        $conn=mysqli_connect($url,$username,$password,"SoilSensors");
+        if(!$conn){
+        die('Could not Connect My Sql:' .mysql_error());
+        }
+      ?>
+
+      <div id="Dashboard" class="tabcontent">        
+      <div class="w3-col s8 w3-left">
+      <div class="table-title">
+      <?php
+        $query="SELECT * FROM SoilSensors.Plots";
+        if ($result = mysqli_query($conn, $query)) {
+        while ($row = mysqli_fetch_row($result)) {
+        $sensor=$row[2];
+        echo $sensor;
+      ?>
+      <div id=>
+      <div>
+      <iframe width="100%" height="450px" 
+      frameborder="0" scrolling="yes" 
+      src= <?php echo $row[3];?>>
+      </iframe>
+      </div>
+      </div>
+      <?php
+        }
+        mysqli_free_result($result);
+        }
+      ?>
+      </div>
+      </div>
+      </div>
+
+
+
+      <div id="Add Sensor" class="tabcontent">
       <div class="w3-col s8 w3-left">
       <div class="table-title">
         Plot
       <div id="grid-1-2">
       <div>
-      <iframe width="100%" height="650px" 
-      frameborder="0" scrolling="yes" 
-      src="http://192.168.1.210:8123/local/SensorSector01Plot.html">
-      </iframe>
+      
+
       </div>
       </div>
       </div>
       </div>
+      </div>
+
+
+
+
+      <div id="Add Plant" class="tabcontent">
+      <div class="w3-col s8 w3-left">
+      <div class="table-title">
+        Plot
+      <div id="grid-1-2">
+      <div>
+      
+
+      </div>
+      </div>
+      </div>
+      </div>
+      </div>
+
+
+
+
+
+      <div id="Assign Sensor" class="tabcontent">
+      <div class="w3-col s8 w3-left">
+      <div class="table-title">
+        Plot
+      <div id="grid-1-2">
+      <div>
+      
+
+      </div>
+      </div>
+      </div>
+      </div>
+      </div>
+
+      <script>
+        // Javascript function to show only selected tab contents
+        function openDash(evt, dashName) {
+            var i, tabcontent, tablinks;
+            tabcontent = document.getElementsByClassName("tabcontent");
+            for (i = 0; i < tabcontent.length; i++) {
+            tabcontent[i].style.display = "none";
+            }
+            tablinks = document.getElementsByClassName("tablinks");
+            for (i = 0; i < tablinks.length; i++) {
+            tablinks[i].className = tablinks[i].className
+                    .replace(" active", "");
+            }
+            document.getElementById(dashName).style.display = "block";
+            evt.currentTarget.className += " active";
+        }
+      </script>
+      <script>
+        // Javascript function to set default selected tab
+        document.getElementById("defaultOpen").click();
+      </script>
     </body>
 </html>
