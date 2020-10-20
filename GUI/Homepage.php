6d3363d1eed475305118a3c77496421227dba43e
cad2040 <cad2040@gmail.com>
2020-10-20 13:15:22 +0100

    Update Homepage.php

--- GUI/Homepage.php
+++ GUI/Homepage.php
@@ -3,6 +3,7 @@
     <head>
         <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
         <meta name="Garden Sensors Dashboard" content="" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Garden Sensors Dashboard</title>
         <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
         <style>
